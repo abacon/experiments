579513855f4ea03ec384db0090eb68cf05a7c08f
Andrew Bacon <abacon@gmail.com>
2014-07-05 20:22:33 -0700

    [openfda] Rewrote to use react, though killed the actual AJAX bit in the process.

--- openfda/openfda.js
+++ openfda/openfda.js
@@ -1,3 +1,66 @@
+var OpenFDABrowser = React.createClass({
+  displayName: "OpenFDABrowser",
+  render: function() {
+    return ReactBootstrap.Grid({fluid: true},
+      ReactBootstrap.Row({},
+        ReactBootstrap.Col({md: 12},
+          BrowserTitle({})
+      )),
+      ReactBootstrap.Row({},
+        ReactBootstrap.Col({md: 4},
+          QueryBuilder({})
+        ),
+        ReactBootstrap.Col({md: 8},
+          ResponseJSONDisplay({}),
+          OpenFDAGraph({})
+        )
+      )
+   );
+  }
+});
+
+var BrowserTitle = React.createClass({
+  displayName: "BrowserTitle",
+  render: function() {
+    return ReactBootstrap.PageHeader({},
+            React.DOM.h1({}, "OpenFDA, meet Plot.ly ",
+              React.DOM.small({}, "Investigate adverse drug event reports"
+           )));
+  }
+});
+
+var QueryBuilder = React.createClass({
+  displayName: "QueryBuilder",
+  render: function() {
+    return ReactBootstrap.Panel({},
+      React.DOM.form({role: "form"},
+        ReactBootstrap.Input({label: "Drug", id: "drug", defaultValue: "fluoxetine", type: "text"}, null)
+      )
+    )
+  }
+});
+
+var ResponseJSONDisplay = React.createClass({
+  displayName: "ResponseJSONDisplay",
+  render: function() {
+    return ReactBootstrap.Panel({header: "Response JSON"}, "???")
+  }
+});
+
+var OpenFDAGraph = React.createClass({
+  displayName: "OpenFDAGraph",
+  render: function() {
+    return ReactBootstrap.Panel({header: "graph dat data"}, "")
+  }
+});
+
+
+React.renderComponent(
+  OpenFDABrowser(),
+  document.getElementById('main')
+);
+
+/**
 $(document).ready(function() {
   $("#refreshButton").bind("click", function(e) {
     var drugInput = $("#drug")[0],
@@ -18,3 +81,4 @@ $(document).ready(function() {
           );
   });
 });
+**/
