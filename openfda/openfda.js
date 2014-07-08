b70965f3874aea747c422f7f09f2a67fae9aa379
Andrew Bacon <abacon@gmail.com>
2014-07-07 21:26:22 -0700

    [openfda] Remove some dead code.

--- openfda/openfda.js
+++ openfda/openfda.js
@@ -138,18 +138,3 @@ React.renderComponent(
   OpenFDABrowser(),
   document.getElementById('main')
 );
-
-/**
-    $.ajax('https://api.fda.gov/drug/event.json',
-           {
-             data: {
-               search: 'patient.drug.openfda.substance_name:"' + drugInput.value + '"',
-               limit: 1
-             },
-             success: function(res, bod) {
-               $(resultsDiv).children().remove();
-               $(resultsDiv).append($("<pre />")).children("pre").append(JSON.stringify(res,null, "\t"));
-             }
-           }
-          );
-**/
